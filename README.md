a08e0c926fad553be7b82726c1cac709cc6c3164
Leah Hartman <lghart@gl-login2.arc-ts.umich.edu>
2025-03-18 13:22:37 -0400

    Changes to README, added .gitignore, and made modifications to profile and job execution files

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # HiPACE++ Utilities
 
-This is where I store all information, files, and examples I've gathered using HiPACE++. This includes profiles for different clusters I've worked on, example decks, as well as an example job script.
+This repository is where I store all decks, analysis scripts/notebooks, and examples I've gathered using HiPACE++. This includes example profiles, decks, and an example job script for the Great Lakes cluster at the University of Michigan.
 
 The file structure is as follows:
-    - /analysis/ --> Holds all analysis scripts
-        - getinfo.py: Gets all information from ...
+```bash
+.
+├── analysis/			# Contains all resources related to data analysis
+│   ├── notebooks/		# Interactive Jupyter Notebooks (walkthroughs, basic analysis, etc.)
+│   └── scripts/		# Automation scripts for analysis tasks
+├── decks/			# Collection of important and interesting input decks
+├── jobex.sh		# Job batch script for running on the Great Lakes cluster at the University of Michigan
+├── profile.hipace	# Environment setup file for HiPACE++ on the Great Lakes cluster
+└── README.md		# Overview of the project, usage instructions, and documentation
+```
+
+# Installation Instructions
+## Great Lakes at the University of Michigan
 
 
-This is still a work in progress!
+
+
--- jobex.sh
+++ jobex.sh
@@ -1,25 +1,57 @@
 #!/bin/bash
 
-#SBATCH --partition=aa100
+# "#SBATCH" directives that convey submission options:
 #SBATCH --job-name=hipace-example
-#SBATCH --output=hipace-example.%j.out
-#SBATCH --qos=normal
-#SBATCH --time=02:00:00
-#SBATCH --qos=normal
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:1
-#SBATCH --mail-user=leah.ghartman@gmail.com
+#SBATCH --ntasks-per-node=2
+#SBATCH --gres=gpu:2
+#SBATCH --mem-per-cpu=1g
+#SBATCH --time=2:00:00
+#SBATCH --account=agrt98
+#SBATCH --export=ALL
+#SBATCH --partition=gpu
 
-# path to executable and input script
-EXE=/projects/leha2242/software/hipace/build/bin/hipace
-INPUTS=inputs_normalized
+#SBATCH --mail-user=lghart@umich.edu
+#SBATCH --mail-type=BEGIN,END
 
-# GPU-aware MPI
-export MPICH_GPU_SUPPORT_ENABLED=1
+# The application(s) to execute along with its input arguments and options:
 
-# expose one GPU per MPI rank
-export CUDA_VISIBLE_DEVICES=$SLURM_LOCALID
+##################################################################################
+# EDIT BELOW HERE
+##################################################################################
 
-# run everything
-srun ${EXE} ${INPUTS}
+# Select the input file, root directory, and data directory
+export INPUTFILE=~/hipaceutils/decks/plasma-prof
+
+export ROOT_DIR=~/hipace
+export DATA_DIR=/scratch/agrt_root/agrt98/lghart/hiresults
+
+# Name of run (OPTIONAL; can be blank, no space)
+RUNTITLE=test
+
+# GPU settings
+export MPICH_GPU_SUPPORT_ENABLED=1		# GPU-aware MPI
+export CUDA_VISIBLE_DEVICES=$SLURM_LOCALID	# Expose one GPU per MPI rank
+
+#################################################################################
+
+# Path to HiPACE++ executable and new directory name
+export EXEC=hipace.MPI.CUDA.DP.LF
+export DIR_NAME=hip_${RUNTITLE}_${SLURM_JOB_ID}%.nyx.engin.umich.edu
+
+# Set directory up to output data
+mkdir ${DATA_DIR}/${DIR_NAME}
+cp -f ${ROOT_DIR}/build/bin/${EXEC} ${DATA_DIR}/${DIR_NAME}
+cp -f ${INPUTFILE} ${DATA_DIR}/${DIR_NAME}/hi-stdin
+
+cd ${DATA_DIR}/${DIR_NAME}
+
+# Add text file for notes
+touch notes
+echo run did not finish >> notes
+
+# Run code
+srun ${EXEC} ${INPUTFILE}
+
+# Remove executable and notes if job ran successfully
+rm -f ${EXEC} notes
